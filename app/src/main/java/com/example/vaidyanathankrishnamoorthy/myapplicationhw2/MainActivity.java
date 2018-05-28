89c1fb63b369d5e17f806adbe826345d676e511d
vaidyanathankrishnamoorthy <vaidy11@gmail.com>
2018-05-28 15:34:23 -0700

    2nd commit

--- app/src/main/java/com/example/vaidyanathankrishnamoorthy/myapplicationhw2/MainActivity.java
+++ app/src/main/java/com/example/vaidyanathankrishnamoorthy/myapplicationhw2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vaidyanathankrishnamoorthy.myapplicationhw2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
